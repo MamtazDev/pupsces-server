cca1a73108b6d19103a0f6176dd2ae45ee9e7ede
Eunice <valdezeunice804@gmail.com>
2024-01-29 08:58:17 +0800

    body parser

--- db.js
+++ db.js
@@ -4,12 +4,10 @@ import mysql from "mysql2/promise";
 dotenv.config();
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_DATABASE || "mydatabase",
-  port: process.env.DB_PORT || 3000,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 export { pool };
