48709c360f605d8f660fa138c0fe41bc87691c30
Eunice <valdezeunice804@gmail.com>
2024-01-29 09:06:58 +0800

    sk

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ import mysql from "mysql2/promise";
 dotenv.config();
 
 const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
