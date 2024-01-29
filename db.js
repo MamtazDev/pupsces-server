543d89599c59046d19dfea1ec536f18da45a4965
Eunice <valdezeunice804@gmail.com>
2024-01-29 08:51:42 +0800

    server

--- db.js
+++ db.js
@@ -1,23 +1,15 @@
-import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import mysql from "mysql2/promise";
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  connectionLimit: 10,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_DATABASE || "mydatabase",
+  port: process.env.DB_PORT || 3306,
 });
 
-// Use the promise method directly on the main mysql2 export
-const createConnection = async () => {
-  return await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  });
-};
-
-export { pool, createConnection };
+export { pool };
