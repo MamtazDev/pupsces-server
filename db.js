01e54a58185be6ed9d89b6c4be74161439af6ced
Eunice <valdezeunice804@gmail.com>
2024-01-29 19:10:35 +0800

    k

--- db.js
+++ db.js
@@ -8,10 +8,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 30000,
+
+  connectionLimit: 1000,
 });
 
 export { pool };
